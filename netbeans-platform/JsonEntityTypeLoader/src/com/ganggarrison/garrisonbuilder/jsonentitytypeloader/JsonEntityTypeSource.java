9f025f96da32b86ba8349e4bda8f99de763e9730
cspotcode <cspotcode@gmail.com>
2011-05-23 22:59:12 -0400

    Started work on loading entity types from a json config and image files.
    Added wrapper modules around the Jackson JSON library.
    Added JsonEntityTypeLoader module, functionality unimplemented.

---
+++ netbeans-platform/JsonEntityTypeLoader/src/com/ganggarrison/garrisonbuilder/jsonentitytypeloader/JsonEntityTypeSource.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ganggarrison.garrisonbuilder.jsonentitytypeloader;
+
+import com.ganggarrison.garrisonbuilder.entitytypesources.EntityTypeSource;
+import com.ganggarrison.garrisonbuilder.gamemap.EntityType;
+
+/**
+ *
+ * @author cspotcode
+ */
+public class JsonEntityTypeSource implements EntityTypeSource {
+
+    @Override
+    public EntityType[] getEntityTypes() {
+        // attempt to load the json config file
+        
+    }
+    
+}
