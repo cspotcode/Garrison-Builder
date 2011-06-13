aa859bfe70aa44bd4a27ff4a9d4addfcf809309e
cspotcode <cspotcode@gmail.com>
2011-06-12 23:54:40 -0400

    bg and wm loading dialogs remember the previously used directory.

--- netbeans-platform/Core/src/com/ganggarrison/garrisonbuilder/OpenImageAction.java
+++ netbeans-platform/Core/src/com/ganggarrison/garrisonbuilder/OpenImageAction.java
@@ -8,7 +8,8 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import org.openide.filesystems.FileChooserBuilder;
 
 /**
  * Abstract Action that prompts the user for an image file, then loads it, then
@@ -19,12 +20,13 @@ public abstract class OpenImageAction implements ActionListener {
 
     @Override
     public final void actionPerformed(ActionEvent ev) {
-        JFileChooser chooser = new JFileChooser();
-        int retVal = chooser.showOpenDialog(null);
-        // don't proceed if user cancelled the dialog
-        if(retVal != JFileChooser.APPROVE_OPTION)
+        File selectedFile = new FileChooserBuilder(OpenImageAction.class)
+                .setFilesOnly(true)
+                .setTitle("Open image")
+                .addFileFilter(new FileNameExtensionFilter("PNG Image", "png"))
+                .showOpenDialog();
+        if(selectedFile == null)
             return;
-        File selectedFile = chooser.getSelectedFile();
         
         // attempt to open the file as an image
         BufferedImage image = null;
@@ -34,6 +36,7 @@ public abstract class OpenImageAction implements ActionListener {
             OutputHelper.getDefault()
                     .println("Error: unable to read image "
                             + selectedFile.getPath());
+            return;
         }
         if(image != null)
             imageOpened(image);
